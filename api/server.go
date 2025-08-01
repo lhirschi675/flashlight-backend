ba53bb288b32650f37d5373bf146638da2148b60
LOGAN HIRSCHI <logan.d@hotmail.com>
2025-07-31 21:26:48 -0400

    updated server to use get/post methods

--- api/server.go
+++ api/server.go
@@ -39,7 +39,7 @@ func NewServer() *Server {
 	s.Use(mux.CORSMethodMiddleware(s.Router))
 	s.Use(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", "*") // or specify frontend origin
+			w.Header().Set("Access-Control-Allow-Origin", "*")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 			if r.Method == http.MethodOptions {
@@ -56,10 +56,19 @@ func NewServer() *Server {
 
 func (s *Server) routes() {
 	s.HandleFunc("/students", s.createStudent()).Methods("POST")
-	s.HandleFunc("/students", s.listStudents()).Methods("GET")
 	s.HandleFunc("/students/{id}", s.getStudent()).Methods("GET")
-	s.HandleFunc("/students/{id}", s.updateStudent()).Methods("PUT")
-	s.HandleFunc("/students/{id}", s.deleteStudent()).Methods("DELETE")
+	s.HandleFunc("/students", s.listStudents()).Methods("GET")
+}
+
+func (s *Server) listStudents() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var students []Student
+		if err := s.db.Find(&students).Error; err != nil {
+			http.Error(w, "Failed to list students", http.StatusInternalServerError)
+			return
+		}
+		json.NewEncoder(w).Encode(students)
+	}
 }
 
 func (s *Server) createStudent() http.HandlerFunc {
@@ -86,17 +95,6 @@ func (s *Server) createStudent() http.HandlerFunc {
 	}
 }
 
-func (s *Server) listStudents() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var students []Student
-		if err := s.db.Find(&students).Error; err != nil {
-			http.Error(w, "Failed to list students", http.StatusInternalServerError)
-			return
-		}
-		json.NewEncoder(w).Encode(students)
-	}
-}
-
 func (s *Server) getStudent() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		id, err := uuid.Parse(mux.Vars(r)["id"])
@@ -112,59 +110,5 @@ func (s *Server) getStudent() http.HandlerFunc {
 		}
 
 		json.NewEncoder(w).Encode(student)
-	}
-}
-
-func (s *Server) updateStudent() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		id, err := uuid.Parse(mux.Vars(r)["id"])
-		if err != nil {
-			http.Error(w, "Invalid UUID", http.StatusBadRequest)
-			return
-		}
-
-		var input Student
-		if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-			http.Error(w, "Invalid input", http.StatusBadRequest)
-			return
-		}
-
-		var student Student
-		if err := s.db.First(&student, "id = ?", id).Error; err != nil {
-			http.NotFound(w, r)
-			return
-		}
-
-		student.Name = input.Name
-		student.Course = input.Course
-		student.Status = input.Status
-		student.Email = input.Email
-		student.Phone = input.Phone
-		student.Age = input.Age
-		student.UpdatedAt = time.Now()
-
-		if err := s.db.Save(&student).Error; err != nil {
-			http.Error(w, "Failed to update student", http.StatusInternalServerError)
-			return
-		}
-
-		json.NewEncoder(w).Encode(student)
-	}
-}
-
-func (s *Server) deleteStudent() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		id, err := uuid.Parse(mux.Vars(r)["id"])
-		if err != nil {
-			http.Error(w, "Invalid UUID", http.StatusBadRequest)
-			return
-		}
-
-		if err := s.db.Delete(&Student{}, "id = ?", id).Error; err != nil {
-			http.Error(w, "Failed to delete student", http.StatusInternalServerError)
-			return
-		}
-
-		w.WriteHeader(http.StatusNoContent)
 	}
 }
